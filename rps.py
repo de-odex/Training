81ceb710349b9d2b842a1584fa37fc0f327485d6
Justin Kyle Ramos <justinfblover@gmail.com>
2015-07-08 21:26:14 +0800

    Updated for python 3.4

--- rps.py
+++ rps.py
@@ -28,49 +28,47 @@ def rps():
         computerguess=random.choice(["rock", "paper", "scissors"]) #Computer Guess
 
         erase()
-        print " You guessed", playerguess
-        print ""
-        print " The computer guessed", computerguess
-        print ""
+        print(" You guessed", playerguess)
+        print("")
+        print(" The computer guessed", computerguess)
+        print("")
 
         #Deciding who won
         if playerguess == computerguess:
-            print " Its a Tie!"
+            print("Its a Tie!")
             ties += 1
         elif winner[playerguess]==computerguess:
-            print " You win!"
+            print("You win!")
             win += 1
         else:
-            print " You Lose"
+            print("You Lose")
             lose += 1
-        print ""
+        print("")
 
         #Play again statment
-        print" Would you like to play again? Yes or No?"
-        answer=raw_input(" >")
+        print("Would you like to play again? Yes or No?")
+        answer=input(">").lower()
         if answer == "no":
             erase()
-            print " Thank you for playing! Here are your results!"
-            print " Total Wins:", win
-            print " Total Loses:",lose
-            print " Total Ties:",ties
+            print(" Thank you for playing! Here are your results!")
+            print(" Total Wins:", win)
+            print(" Total Loses:",lose)
+            print(" Total Ties:",ties)
             keep_playing=False
 
 
 #Welcome Text
-print """
-    Welcome to Rock, Paper, Scissors!
-    ---------------------------------
-    Where Rock Beats Scissors,
-    Scissors Cuts Paper,
-    And Paper Covers Rock!
-    ---------------------------------
-    Would you like to play? Yes or No?
-"""
-play=raw_input(" >").lower()
+print("Welcome to Rock, Paper, Scissors!")
+print("---------------------------------")
+print("Where Rock Beats Scissors,")
+print("Scissors Cuts Paper,")
+print("And Paper Covers Rock!")
+print("---------------------------------")
+print("Would you like to play? Yes or No?")
+play=input(">").lower()
 if play=="yes":
     rps()
 else:
-    print "Oh well!"
+    print("Oh well!")
 
 
